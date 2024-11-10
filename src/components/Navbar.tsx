dc62863942148f35e027c0e4833e46a1f2c893a5
freelancing with programming <iosattbsoft2021@gmail.com>
2024-11-10 15:32:32 +0500

    vv

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,32 @@
-
-import { useState } from 'react'
-import  styles from './Navbar.module.css';
+import React from "react";
+import styles from './Navbar.module.css';
 import { Link } from "react-router-dom";
-
-function Navbar() {
-    // adding the states 
-    const [isActive, setIsActive] = useState(false);
-    //add the active class
-    const toggleActiveClass = () => {
-      setIsActive(!isActive);
-    };
-    //clean up function to remove the active class
-    const removeActive = () => {
-      setIsActive(false)
-    }
+const Navbar = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <nav className={`${styles.navbar}`}>
-            {/* logo */}
-            <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
-              <li onClick={removeActive}>
-              <Link to="/">Home</Link>
-              </li>
-              <li onClick={removeActive}>
-              <Link to="/about">About</Link>
-              </li>
-              <li onClick={removeActive}>
-              <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-            <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`}  onClick={toggleActiveClass}>
-              <span className={`${styles.bar}`}></span>
-              <span className={`${styles.bar}`}></span>
-              <span className={`${styles.bar}`}></span>
-            </div>
-          </nav>
-        </header>
-      </div>
+        <>
+            <div className={styles.Nav}>
+                <div className={styles.Bars}>
+
+                <div className={styles.NavMenu}>
+                    <Link className={styles.NavLinks} to="/about" >
+                        About
+                    </Link>
+                    <Link className={styles.NavLinks} to="/Contactus">
+                        Contactus
+                    </Link>
+                    <Link className={styles.NavLinks} to="/">
+                        Home
+                    </Link>
+                    {/* Second Nav */}
+                    {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+                </div>
+                </div>
+                    <Link className={styles.NavBtnLink} to="/signin">
+                        Sign In
+                    </Link>
+                </div>
+        </>
     );
-  }
-  export default Navbar;
-  ;
+};
+
+export default Navbar;
